20941572651a11fd4d08217a9d6c69ce348eddb4
Henry Latte <HLatte@users.noreply.github.com>
2024-10-31 17:55:28 +0000

    lala.py

---
+++ volatility/lala.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='volatility-trading',
+    version='0.0.1',
+    url='https://github.com/jasonstrimpel/volatility-trading/',
+    license='GPL-3.0-or-later',
+    packages=['volatility','volatility/models'],
+)
